988ade6ce6d497f72e6fb828ca1ed5e79ca591a7
dkekel <dkekel@gmail.com>
2020-06-06 19:23:47 +0200

    Move profile matcher logic to Vue

--- phunter/src/Matcher.vue
+++ phunter/src/Matcher.vue
@@ -1,59 +1,216 @@
 <template>
     <div class="container">
-        <div class="container">
-            <div class="text-center">
-                <h2 class="phunter-heading">PHunter</h2>
-                <h5 class="codename-heading"><i>(codename: Fibonacci Girls)</i></h5>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">API Token</span>
-                    </div>
-                    <input id="api-token" type="text" class="form-control" placeholder="API Token" aria-label="Username"
-                           aria-describedby="basic-addon1" value="f4aecb01-c26a-4db7-a977-4be1d03a64c7">
-                    <div class="input-group-append">
-                        <button class="btn btn-success" type="button" onclick="init()">Start</button>
+        <div class="row">
+            <div class="col-8">
+                <div class="container">
+                    <div class="text-center">
+                        <div id="label-container" class="alert alert-info">
+                            <div v-for="(prediction, idx) in predictionResults" role="progressbar"
+                                 class="progress-bar" :class="idx === 0 ? 'bg-success' : 'bg-warning'"
+                                 :aria-valuenow="prediction.classResult" aria-valuemin="0" aria-valuemax="100"
+                                 style="height: 20px" :style="{width: prediction.classResult + '%'}">
+                                {{prediction.className}}: {{prediction.classResult.toFixed(2)}}%
+                            </div>
+                        </div>
+                        <img class="img-thumbnail rounded"
+                             src=""
+                             ref="profileImg"
+                             width="224"
+                             height="224"
+                             alt="Image for prediction"/>
                     </div>
                 </div>
-                <div id="label-container" class="alert alert-info"></div>
-                <img id="test-img" class="recognition-image img-thumbnail rounded"/>
+                <div class="container">
+                    <div id="user-thumbnails" class="text-center">
+                        <div v-for="thumbnail in thumbnails" class="d-inline p-2">
+                            <img class="profile-thumbnail img-thumbnail rounded"
+                                 :src="thumbnail.src"
+                                 :title="'Pretty' + thumbnail.probability"
+                                 alt="Predicted image thumbnail"/>
+                        </div>
+                    </div>
+                </div>
+                <div ref="infoLog" class="container logs overflow-auto">
+                    <samp v-for="log in logs" class="d-block" v-html="log"></samp>
+                </div>
             </div>
-        </div>
-        <div class="container">
-            <div id="user-thumbnails" class="text-center">
+            <div class="col">
+                <MatcherConfig v-on:start-hunt="hunt" v-on:stop-hunt="stopHunt"/>
             </div>
-        </div>
-        <div id="info-log" class="container logs overflow-auto">
         </div>
     </div>
 </template>
 
 <script>
+  import axios from "axios";
+  import * as tf from "@tensorflow/tfjs";
+  import * as tmImage from "@teachablemachine/image";
+  import MatcherConfig from "./components/MatcherConfig";
+
+  const URL = "https://teachablemachine.withgoogle.com/models/8mc6xcvVm/";
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  const fibonacciMapping = {"0": 0, "1": 0.1, "2": 0.2, "3": 0.3, "5": 0.6, "8": 0.8, "PARFAIT": 1};
+
   export default {
     name: "Matcher",
+    components: {MatcherConfig},
     props: {
       apiToken: String
+    },
+    data() {
+      return {
+        model: null,
+        maxPredictions: 0,
+        profiles: [],
+        thumbnails: [],
+        result: [],
+        totalScore: [],
+        logs: [],
+        started: false,
+        feedProcessed: false
+      }
+    },
+    mounted() {
+      this.loadModel();
+    },
+    updated() {
+        const logEl = this.$refs.infoLog;
+        logEl.scrollTop = logEl.scrollHeight;
+    },
+    watch: {
+      feedProcessed: function (val, oldVal) {
+        if (!oldVal && val && this.started) {
+          this.hunt();
+        }
+      }
+    },
+    computed: {
+      predictionResults: function () {
+        let totalCount = 0;
+        const perClassTotal = new Array(this.maxPredictions).fill(0);
+        const results = {}
+        for (let faceResult of this.result) {
+          totalCount++;
+          const imageResult = faceResult.prediction;
+          for (let i = 0; i < this.maxPredictions; i++) {
+            let classTotal = perClassTotal[i];
+            classTotal += imageResult[i].probability;
+            perClassTotal[i] = classTotal;
+            const classNormalized = (classTotal / totalCount) * 100;
+            const className = imageResult[i].className;
+            results[className] = classNormalized;
+          }
+        }
+        return Object.keys(results).map(key => {return {className: key, classResult: results[key]}});
+      }
+    },
+    methods: {
+      async loadModel() {
+        this.model = await tmImage.load(modelURL, metadataURL);
+        this.maxPredictions = this.model.getTotalClasses();
+      },
+      getTimestamp() {
+        return new Date().toLocaleString();
+      },
+      async hunt() {
+        this.result = [];
+        this.thumbnails = [];
+        this.started = true;
+        this.feedProcessed = false;
+        this.appendFeedLoadingLog();
+        await this.loadProfiles();
+        while (this.started && this.profiles.length > 0) {
+          const profile = this.profiles.pop();
+          await this.analyzeProfile(profile);
+          this.thumbnails = [];
+          this.result = [];
+        }
+        this.feedProcessed = true;
+      },
+      async loadProfiles() {
+        const response = await axios.get("http://localhost:3000/feed",
+          {headers: {"Api-Token": this.apiToken}});
+        this.profiles = response.data.users;
+        this.appendFeedSizeLog(this.profiles.length);
+      },
+      async analyzeProfile(profile) {
+        const userId = profile.userId;
+        const userName = profile.userName;
+        const images = profile.faces;
+        for (let image of images) {
+          await this.setImageForRecognition(userId, image.face);
+          await tf.nextFrame().then(async () => {
+            const imageResult = await this.model.predict(this.$refs.profileImg);
+            this.result.push({face: image.face, file: image.file, prediction: imageResult});
+            this.thumbnails.push(
+              {
+                src: `data:image/png;base64,${image.face}`,
+                probability: imageResult[0].probability
+              }
+            );
+          });
+        }
+        await this.categorizeResult({user: userId, result: this.result}, userName);
+      },
+      setImageForRecognition(userId, image) {
+        return new Promise(resolve => {
+          const profileImg = this.$refs.profileImg;
+          profileImg.onload = () => {
+            resolve();
+          }
+          profileImg.src = `data:image/png;base64,${image}`;
+        });
+      },
+      async categorizeResult(result, userName) {
+        const response = await axios.post('http://localhost:3000/categorize', result,
+          {headers: {"Api-Token": this.apiToken}});
+        const userScore = response.data.userScore;
+        this.appendUserTotalLog(userScore, userName);
+      },
+      appendFeedLoadingLog() {
+        const logLine = this.appendLogTimestamp('Loading next candidates... Espera por favor!');
+        this.logs.push(logLine);
+      },
+      appendFeedSizeLog(candidatesCount) {
+        const logLine =
+          this.appendLogTimestamp(`Successfully loaded <b>${candidatesCount}</b> candidates with faces!`);
+        this.logs.push(logLine);
+      },
+      appendUserTotalLog(prettyScore, userName) {
+        const roundedScore = parseFloat(prettyScore.toFixed(1));
+        let fibonacciScore;
+        for (let key in fibonacciMapping) {
+          if (roundedScore <= fibonacciMapping[key]) {
+            fibonacciScore = key;
+            break;
+          }
+        }
+        const logLine = this.appendLogTimestamp(`<b>${userName}</b> score: ${fibonacciScore}`);
+        this.logs.push(logLine);
+      },
+      appendLogTimestamp(logMessage) {
+        return `${this.getTimestamp()} ${logMessage}`;
+      },
+      stopHunt() {
+        this.started = false;
+        this.appendStopLog();
+      },
+      appendStopLog() {
+        const logLine = this.appendLogTimestamp('Stopping matching process...');
+        this.logs.push(logLine);
+      }
     }
   }
 </script>
 
 <style scoped>
-    .recognition-image {
-        height: 250px;
-    }
-
     .profile-thumbnail {
         height: 100px;
     }
 
     .logs {
         height: 150px;
-    }
-
-    .phunter-heading {
-        font-weight: 300;
-    }
-
-    .codename-heading {
-        font-weight: 100;
     }
 </style>
---
+++ phunter/src/components/MatcherConfig.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Matcher Config</h5>
+            </div>
+            <div class="card-body">
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <button class="btn btn-success" @click="hunt">Hunt</button>
+                    </div>
+                    <div class="col-sm-6">
+                        <button class="btn btn-warning" @click="stop">Stop</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "MatcherConfig",
+    methods: {
+      hunt() {
+        this.$emit('start-hunt');
+      },
+      stop() {
+        this.$emit('stop-hunt');
+      }
+    }
+  }
+</script>
